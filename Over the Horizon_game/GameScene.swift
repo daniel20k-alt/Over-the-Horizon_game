6a86dd1697c60f53bf17497c001e3e1caab6471b
Testing_author <testing_beta@mail.com>
2020-10-14 09:36:18 +0300

    Configuring the topSky() and bottomSky() and placing them on the screen.

--- Over the Horizon_game/GameScene.swift
+++ Over the Horizon_game/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         createPlayer()
+        createSky()
         
     }
     
@@ -36,5 +37,23 @@ class GameScene: SKScene {
         let runForever = SKAction.repeatForever(animation)
         
         player.run(runForever)
+    }
+    
+   func createSky() {
+//        let topSky = SKSpriteNode(color: UIColor(hue: 0.66, saturation: 0.14, brightness: 0.97, alpha: 1))
+    let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.67))
+    topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+    
+    let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.33))
+    bottomSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+    
+    topSky.position = CGPoint(x: frame.midX, y: frame.height)
+    bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height)
+    
+    addChild(topSky)
+    addChild(bottomSky)
+    
+    topSky.zPosition = -40
+    bottomSky.zPosition = -40
     }
 }
